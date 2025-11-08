4b18d3aabf11e840a60f82eec004d53b531bbee3
210010010064-vinay <210010010064@iite.ac.in>
2025-11-08 19:10:28 +0530

    Updated

--- src/constant/prediction_pipeline/__init__.py
+++ src/constant/prediction_pipeline/__init__.py
@@ -2,6 +2,6 @@ import os
 
 PRED_SCHEMA_FILE_PATH = os.path.join('config', 'prediction_schema.yml')
 
-PREDICTION_MODEL_FILE_NAME = r'C:\Users\Student\Desktop\Spam_Detection\src\artifacts\10_09_2025_10_53_55\model_trainer\trained_model\model.pkl'
+PREDICTION_MODEL_FILE_NAME = r'src\artifacts\10_09_2025_10_53_55\model_trainer\trained_model\model.pkl'
 PREDICTION_INPUT_FILE_NAME = "customer_pred_data.csv"
 PREDICTION_OUTPUT_FILE_NAME = "customer_predictions.csv"
